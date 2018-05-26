ae0ac63fa43a32040933e89c3d8c10608caaeadc
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-05-26 15:03:22 +0600

    Routes is added

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,31 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Users Info
+Route::get('home', 'UsersController@index');
+Route::get('home/{id}', 'UsersController@show');
+Route::post('home', 'UsersController@store');
+Route::put('home', 'UsersController@store');
+Route::delete('home/{id}', 'UsersController@destroy');
+
+//Users Debit
+Route::get('debit', 'DebitController@index');
+Route::get('debit/{id}', 'DebitController@show');
+Route::post('debit', 'DebitController@store');
+Route::put('debit', 'DebitController@store');
+Route::delete('debit', 'DebitController@destroy');
+
+//Users Credit
+Route::get('credit', 'CreditController@index');
+Route::get('credit/{id}', 'CreditController@show');
+Route::post('credit', 'CreditController@store');
+Route::put('credit', 'CreditController@store');
+Route::delete('credit', 'CreditController@destroy');
+
+//Admin Info
+Route::get('admin', 'AdminController@index');
+Route::get('admin/{id}', 'AdminController@show');
+Route::post('admin', 'AdminController@store');
+Route::put('admin', 'AdminController@store');
+Route::delete('admin', 'AdminController@destroy');
