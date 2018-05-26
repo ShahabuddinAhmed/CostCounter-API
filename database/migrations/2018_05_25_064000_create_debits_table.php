ef0bed80fcad708f5ef463ce5c9405283892ace8
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-05-26 16:24:22 +0600

    debitsTable is created

---
+++ database/migrations/2018_05_25_064000_create_debits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDebitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('debits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("date");
+            $table->string("name");
+            $table->integer("debit");
+            $table->integer("users_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('debits');
+    }
+}
